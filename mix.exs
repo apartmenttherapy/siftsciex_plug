837214e394fd9103f7147b301763f168ce78c5de
Glen Holcomb <MentalLabor@gmail.com>
2018-07-06 09:27:23 -0600

    Package details

--- mix.exs
+++ mix.exs
@@ -6,9 +6,12 @@ defmodule SiftsciexPlug.MixProject do
       app: :siftsciex_plug,
       version: "0.1.0",
       elixir: "~> 1.6",
+      description: description(),
+      package: package(),
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: [extras: ["README.md"]]
     ]
   end
 
@@ -28,6 +31,19 @@ defmodule SiftsciexPlug.MixProject do
     ]
   end
 
+  defp description do
+    "A Plug for handling Sift Science Web Hooks"
+  end
+
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp package do
+    [
+      licenses: ["LGPLv3"],
+      maintainers: ["Glen Holcomb"],
+      links: %{"GitHub" => "https://github.com/apartmenttherapy/siftsciex_plug"},
+      source_url: "https://github.com/apartmenttherapy/siftsciex_plug"
+    ]
+  end
 end
