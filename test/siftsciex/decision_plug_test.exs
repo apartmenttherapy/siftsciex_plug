c5f5451451f0c1a27140836074dc686e88a925bf
Glen Holcomb <MentalLabor@gmail.com>
2018-08-21 09:54:30 -0600

    Add tests for Parser case

--- test/siftsciex/decision_plug_test.exs
+++ test/siftsciex/decision_plug_test.exs
@@ -7,53 +7,117 @@ defmodule Siftsciex.DecisionPlugTest do
   alias Siftsciex.DecisionPlug
   alias Siftsciex.Plug.TestHandler
 
-  test "If the request is signed it is allowed" do
-    result =
-      "test"
-      |> req(decision())
-      |> put_req_header("x-sift-science-signature", signature())
-      |> DecisionPlug.call(%{"test" => {TestHandler, :run}})
+  describe "Without a Plug.Parser" do
+    test "If the request is signed it is allowed" do
+      result =
+        "test"
+        |> req(decision())
+        |> put_req_header("x-sift-science-signature", signature())
+        |> DecisionPlug.call(%{"test" => {TestHandler, :run}})
 
-    assert result.status() == 200
+      assert result.status() == 200
+    end
+
+    test "If the request is not signed it returns a 403 response" do
+      result =
+        "test"
+        |> req(decision())
+        |> DecisionPlug.call(%{"test" => {TestHandler, :run}})
+
+      assert result.status() == 403
+    end
+
+    test "If the request has an invalid signature then it returns a 403 response" do
+      result =
+        "test"
+        |> req(decision())
+        |> put_req_header("x-sift-science-signature", "bogus")
+        |> DecisionPlug.call(%{"test" => {TestHandler, :run}})
+
+      assert result.status() == 403
+    end
+
+    test "If the request is to a known path it is processed" do
+      result =
+        "test"
+        |> req(decision())
+        |> put_req_header("x-sift-science-signature", signature())
+        |> DecisionPlug.call(%{"test" => {TestHandler, :run}})
+
+      assert result.status() == 200
+    end
+
+    test "If the request is to an unknown path it returns a 404 response" do
+      result =
+        "random"
+        |> req(decision())
+        |> put_req_header("x-sift-science-signature", signature())
+        |> DecisionPlug.call(%{"test" => {TestHandler, :run}})
+
+      assert result.status() == 404
+    end
   end
 
-  test "If the request is not signed it returns a 403 response" do
-    result =
-      "test"
-      |> req(decision())
-      |> DecisionPlug.call(%{"test" => {TestHandler, :run}})
+  describe "With a Plug.Parser" do
+    test "If the request is signed it is allowed" do
+      result =
+        "test"
+        |> req(decision())
+        |> struct(body_params: parsed_body())
+        |> assign(:siftsciex_sig, true)
+        |> DecisionPlug.call(%{"test" => {TestHandler, :run}})
 
-    assert result.status() == 403
+      assert result.status() == 200
+    end
+
+    test "If the request is not signed it returns a 403 response" do
+      result =
+        "test"
+        |> req(decision())
+        |> struct(body_params: parsed_body())
+        |> DecisionPlug.call(%{"test" => {TestHandler, :run}})
+
+      assert result.status() == 403
+    end
+
+    test "If the request has an invalid signature then it returns a 403 response" do
+      result =
+        "test"
+        |> req(decision())
+        |> struct(body_params: parsed_body())
+        |> assign(:siftsciex_sig, false)
+        |> DecisionPlug.call(%{"test" => {TestHandler, :run}})
+
+      assert result.status() == 403
+    end
+
+    test "If the request is to a known path it is processed" do
+      result =
+        "test"
+        |> req(decision())
+        |> struct(body_params: parsed_body())
+        |> assign(:siftsciex_sig, true)
+        |> DecisionPlug.call(%{"test" => {TestHandler, :run}})
+
+      assert result.status() == 200
+    end
+
+    test "If the request is to an unknown path it returns a 404 response" do
+      result =
+        "random"
+        |> req(decision())
+        |> struct(body_params: parsed_body())
+        |> assign(:siftsciex_sig, true)
+        |> DecisionPlug.call(%{"test" => {TestHandler, :run}})
+
+      assert result.status() == 404
+    end
   end
 
-  test "If the request has an invalid signature then it returns a 403 response" do
-    result =
-      "test"
-      |> req(decision())
-      |> put_req_header("x-sift-science-signature", "bogus")
-      |> DecisionPlug.call(%{"test" => {TestHandler, :run}})
+  def parsed_body do
+    {:ok, body} = Poison.decode(decision())
 
-    assert result.status() == 403
-  end
-
-  test "If the request is to a known path it is processed" do
-    result =
-      "test"
-      |> req(decision())
-      |> put_req_header("x-sift-science-signature", signature())
-      |> DecisionPlug.call(%{"test" => {TestHandler, :run}})
-
-    assert result.status() == 200
-  end
-
-  test "If the request is to an unknown path it returns a 404 response" do
-    result =
-      "random"
-      |> req(decision())
-      |> put_req_header("x-sift-science-signature", signature())
-      |> DecisionPlug.call(%{"test" => {TestHandler, :run}})
-
-    assert result.status() == 404
+    body
   end
 
   def req(path, body \\ nil) do
